5ac777ff8cdb0c42cdfb18ecf09bdfe74cf56226
Maximilian Wahl <mexx.wale@gmail.com>
2019-10-06 09:45:36 +0200

    added missing rest-path

--- jpademo/src/main/java/at/htl/jpademo/rest/PersonResource.java
+++ jpademo/src/main/java/at/htl/jpademo/rest/PersonResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
+@Path("persons")
 public class PersonResource {
     @Inject
     PersonFacade personFacade;
