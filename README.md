716012679f0f156de3da0381e920190c02588169
Maximilian Wahl <mexx.wale@gmail.com>
2019-11-16 13:26:49 +0100

    added doc, more data and tests

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # 01-assignment-quarkus-jpa-Maxwahl
+
+## Goal of this exercise
+
+Goal of this exercise was to demonstrate how to use quarkus in combination with a datasource
+and trying to containerize the whole affair.
+
+## Model
+
+The data-model in this exercise resembles a small library.
+
+The library has *Exemplar*s of different *Item*s ( *CD*s or *Book*s in stock).
+A *Person* can make *Loan* containing various *Exemplar*s.
+
+### ERD
+
+<img src ="./images/ERD.png">
+
+### CLD
+
+<img src ="./images/CLD.png">
+
+## Running the project
+
+Running the projects requires following steps in order
+
+1. dockerize PostgreSQL database (docker-compose.yml in repo-root)
+2. execute buildlibrary.sh
+3. execute runlibrary.sh
--- jpademo/src/test/java/at/htl/jpademo/LoanResourceTest.java
+++ jpademo/src/test/java/at/htl/jpademo/LoanResourceTest.java
@@ -4,9 +4,36 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class LoanResourceTest {
+    //just some tests
+    //should only be run when not compiling for docker, otherwise would always fail because of network name of datasource
+    /*
+    @Test
+    public void testLoansSize() {
+        given()
+                .when().get("/loans")
+                .then()
+                .statusCode(200)
+                .body("size()", is(2));
+    }
+    @Test
+    public void testPersonsName() {
+        given()
+                .when().get("/persons/1")
+                .then()
+                .statusCode(200)
+                .body("name", is("Meier"));
+    }
+    @Test void testLoanExemplars(){
+        given()
+                .when().get("/loans/1")
+                .then()
+                .statusCode(200)
+                .body("exemplars.weariness",hasItems("undamaged", "used"));
+    }*/
 
 }
