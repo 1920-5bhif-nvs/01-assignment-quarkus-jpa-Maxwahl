0be866e01122fe80f329c3c34ac1ceb55de81d19
Maximilian Wahl <mexx.wale@gmail.com>
2019-11-16 14:30:59 +0100

    discovered way to set application properties for testing

--- jpademo/src/test/java/at/htl/jpademo/LoanResourceTest.java
+++ jpademo/src/test/java/at/htl/jpademo/LoanResourceTest.java
@@ -9,9 +9,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class LoanResourceTest {
-    //just some tests
-    //should only be run when not compiling for docker, otherwise would always fail because of network name of datasource
-    /*
+    //just some tests    
     @Test
     public void testLoansSize() {
         given()
@@ -34,6 +32,6 @@ public class LoanResourceTest {
                 .then()
                 .statusCode(200)
                 .body("exemplars.weariness",hasItems("undamaged", "used"));
-    }*/
+    }
 
 }
